04e04ce3f4c956d88457ab6443b25e92b5f24275
freakamy <freak.amit1@gmail.com>
2023-07-19 20:53:08 +0530

    Fixed issues

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -18,7 +18,7 @@ const [text, setText] = useState('');
 
     }
 
-    const cleatTextClick = () =>{
+    const clearTextClick = () =>{
         let newText = '';
         setText(newText);
         props.showTheAlert('Text cleared', 'warning')
@@ -42,6 +42,8 @@ const [text, setText] = useState('');
         setText(newText + event.target.value);
     }
 
+    const characterCount = text.replace(/\s/g, '').length
+    const wordCounter = text.split(' ').filter((element) =>{return element.length !== 0}).length
  
 
 
@@ -49,20 +51,20 @@ const [text, setText] = useState('');
     <div className="container mt-3" style={{color: props.mode === 'light' ? 'dark' : 'light'}}>
         <h3>{props.heading}</h3>
         <div className="mb-3">
-        <textarea style={{backgroundColor: props.mode === 'light' ? '#ffffff' : '#212529', color: props.mode === 'light' ? 'black' : 'white'}} className="form-control" value={text} onChange={handleOnChange} id="textarea" rows="8"></textarea>
-        <button className="btn btn-primary mt-3" onClick={upperCaseClick}>UPPERCASE</button>
-        <button className="btn btn-primary mt-3 ms-2" onClick={lowerCaseClick}>LOWECASE</button>
-        <button className="btn btn-primary mt-3 ms-2" onClick={cleatTextClick}>CLEAR TEXT</button>
-        <button className="btn btn-primary mt-3 ms-2" onClick={copyTextClick}>COPY TEXT</button>
-        <button className="btn btn-primary mt-3 ms-2" onClick={removeSpaceClick}>REMOVE SPACE</button>
+        <textarea style={{backgroundColor: props.mode === 'dark' ? '#212529' : 'white', color: props.mode === 'dark' ? 'white' : 'black'}} className="form-control" value={text} onChange={handleOnChange} id="textarea" rows="8"></textarea>
+        <button disabled={text.length===0} className="btn btn-primary mt-3" onClick={upperCaseClick}>UPPERCASE</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 ms-2" onClick={lowerCaseClick}>LOWECASE</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 ms-2" onClick={clearTextClick}>CLEAR TEXT</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 ms-2" onClick={copyTextClick}>COPY TEXT</button>
+        <button disabled={text.length===0} className="btn btn-primary mt-3 ms-2" onClick={removeSpaceClick}>REMOVE SPACE</button>
     </div>  
     <div className="container-mt-5">
         <h4>Text Summary</h4>
-        <p>{text.split(/\s/).length} Words and {text.length} Character</p>
+        <p>{wordCounter} Words and {characterCount} Character</p>
 
         <h3>Preview your Text here</h3>
         <details>
-            <summary>{text.length > 0 ? 'Preview' : 'Enter text to preview here'}</summary>
+            <summary>{text.length > 0 ? 'Preview' : 'Nothing to preview'}</summary>
             <p>{text}</p>
         </details>
     </div>
